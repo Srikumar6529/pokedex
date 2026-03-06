dac8e4396ce1fb3f61fd9c58c0b96e63b1879781
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-03-05 18:46:04 -0600

    added log

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 import (
 	"fmt"
 	"bufio"
+	"os"
 )
 
 func main(){
 	scanner := bufio.NewScanner(os.Stdin)
-	for{
-		fmt.Print("Pokedex")
-		inputString := scanner.Scan().Text()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		
+		inputString := scanner.Text()
 		firstWord := cleanInput(inputString)[0]
+		fmt.Println("Your command was:",firstWord)
 	}
 }
 
