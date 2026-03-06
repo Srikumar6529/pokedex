76216f8aac05ba0ca48c3c472e760f1c75719f51
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-03-05 19:08:13 -0600

    added commnds functionality and help,exit command

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
 
-func main(){
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		
-		inputString := scanner.Text()
-		firstWord := cleanInput(inputString)[0]
-		fmt.Println("Your command was:",firstWord)
-	}
+func main() {
+	startRepl()
 }
-
